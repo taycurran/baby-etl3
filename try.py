d2afa5e06cd4724f68cc6dea4fef1b5496403f98
taycurran <taylorcurranagtc@gmail.com>
2020-05-19 11:54:12 -0500

    Seeing Error in Connection

--- try.py
+++ try.py
@@ -4,15 +4,12 @@ load_dotenv()
 
 import pymysql.cursors
 
-
 labs_conn = pymysql.connect(host=os.environ["LABS_RDS_ENDPOINT"],
                             user=os.environ["LABS_USERNAME"],
                             password=os.environ["LABS_PASSWORD"],
                             database=os.environ["LABS_DB_NAME"],
                             port=os.environ["LABS_PORT"]
                             )
-
-
 
 labs_conn.close()
 print("Done.")
